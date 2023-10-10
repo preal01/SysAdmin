7c4a2b76d07afe17dd75392bc29ef0fa29e640eb
preal01 <jouanneau.warren@gmail.com>
2023-10-10 18:26:03 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-user="$(whoami)"
-su
-apt install sudo
-usermod --append -G sudo $user
-su $user
+user="$(whoami)" &&
+su root && apt install sudo && usermod --append -G sudo $user
+su $user &&
